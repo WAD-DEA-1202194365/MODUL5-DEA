15ab62ba5d6ed639335fbee4ec32185b25e2178e
dea <dhesihotang14@gmail.com>
2021-12-12 23:28:38 +0700

    Create navbar.blade.php

---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,21 @@
+      <!-- Navbar -->
+     <div class="d-flex justify-content-center bg-white">
+          <nav class="navbar navbar-expand-lg navbar-light d-flex justify-content-center">
+               <div class="container-fluid">
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                         <ul class="navbar-nav">
+                              <li class="nav-item">
+                                   <a class="nav-link"  href="/">Home</a>
+                              </li>
+                              <li class="nav-item">
+                                   <a class="nav-link"  href="/vaccine">Vaccine</a>
+                              </li>
+                              <li class="nav-item">
+                                   <a class="nav-link"  href="/patient">Patient</a>
+                              </li>
+                         </ul>
+                    </div>
+               </div>
+          </nav>
+     </div>
+     <!-- Akhir Navbar -->
