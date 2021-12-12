c0fcbb4e3fec59671f1e9b66fb7f626b23cbe47f
dea <dhesihotang14@gmail.com>
2021-12-12 23:30:34 +0700

    Create vaccine.blade.php

---
+++ resources/views/vaccine.blade.php
@@ -0,0 +1,12 @@
+
+
+@extends('layouts.main')
+
+
+@section('container')
+    <center>
+        <a href="createv" type="button" class="btn btn-primary">Add Vaccine</a>
+    </center>
+
+@endsection
+
