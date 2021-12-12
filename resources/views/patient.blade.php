e6c5871929371c0b880259bb404e2ec1051b1e93
dea <dhesihotang14@gmail.com>
2021-12-12 23:30:29 +0700

    Create patient.blade.php

---
+++ resources/views/patient.blade.php
@@ -0,0 +1,10 @@
+
+
+@extends('layouts.main')
+
+
+@section('container')
+        <center>
+        <a href="createp" class="btn btn-primary" type="button">Register Patient</a>
+    </center>
+@endsection
